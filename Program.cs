0036b11144640ea2c5a42685847e03b4092b6940
Tyler Jowers <jowerstc@outlook.com>
2023-01-02 17:22:01 -0500

    initial

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System.CommandLine;
+
+namespace cscli;
+
+class Program
+{
+    static int Main(string[] argv)
+    {
+        /*
+            Program Root Command.
+        */
+        var program = new RootCommand("Display a file.");
+
+        /*
+            Create Options.
+        */
+        var fileOption = new Option<FileInfo?>(
+            name: "--file",
+            description: "File to display."
+        );
+
+        /*
+            Add Options.
+        */
+        program.AddOption(fileOption);
+
+        /*
+            Set Command Handlers.
+        */
+        program.SetHandler((file) =>
+        {
+            DisplayFile(file!);
+        }, fileOption);
+
+
+        /*
+            Invoke CLI.
+        */
+        return program.Invoke(argv);
+    }
+
+    static void DisplayFile(FileInfo file)
+    {
+        File.ReadLines(file.FullName).ToList()
+        .ForEach(line => Console.WriteLine(line));
+    }
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+
+# Setting Up a CLI App on Linux 
+Install .NET software with package manager.
+
+Link https://learn.microsoft.com/en-us/dotnet/core/install/linux
+
+Init a directory with .NET CLI
+```
+$ dotnet new console --framework net6.0
+```
+
+Add System.CommandLine package.
+```
+$ dotnet add package System.CommandLine --prerelease
+```
+
+Build Application. The name comes from the main namespace.
+```
+$ dotnet build
+# ./bin/debug/net6.0/<appname>
+```
